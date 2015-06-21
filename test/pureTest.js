431f1afc3b31f1e71712af0d33c58e8f80ced287
Imre Fazekas <imre.fazekas@icloud.com>
2015-06-21 16:18:21 +0200

    refinements

--- test/pureTest.js
+++
@@ -1,6 +0,0 @@
-var Main = require('../lib/Main');
-
-var fs = require('fs');
-
-var jadeContent = fs.readFileSync( 'test/content.jade', { encoding: 'utf8' });
-console.log( Main( jadeContent, { pretty: true, context: true, indent_size: 1, indent_char: '\t' } ) );
